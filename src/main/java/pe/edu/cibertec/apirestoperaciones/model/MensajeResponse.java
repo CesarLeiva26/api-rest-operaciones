9bee768ccda39ae22c6a21a8b407d3f52927c855
CesarLeiva26 <cesaremanuelleivajuro@gmail.com>
2023-09-28 11:56:42 -0500

    Api rest operaciones

---
+++ src/main/java/pe/edu/cibertec/apirestoperaciones/model/MensajeResponse.java
@@ -0,0 +1,11 @@
+package pe.edu.cibertec.apirestoperaciones.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class MensajeResponse {
+
+    private String resultado;
+}
